ea68c00e3fc39618835771f5c3fcee46c059d015
LD3il <117454985+lucasDubourdieu2@users.noreply.github.com>
2022-11-07 10:24:31 +0000

    Exercice 2 partie 4

--- point/Point.java
+++ point/Point.java
@@ -14,4 +14,18 @@ public class Point {
   public void afficher () {
     System.out.println ("Coordonnées " + x + " " + y) ;
   }
+
+  public int normeMax() {
+    int a = Math.abs(this.x);
+    int b = Math.abs(this.y);
+    
+    return (a >= b ? a:b);
+  }
+
+    public static int normeMax(int x, int y) {
+      int a = Math.abs(x);
+      int b = Math.abs(y);
+      
+      return (a >= b ? a:b);
+  }
 }
--- point/TestPoint.java
+++ point/TestPoint.java
@@ -5,6 +5,8 @@ public class TestPoint {
   public static void main(String[] args) {
     Point point = new Point(2, 3);
     point.afficher();
+    System.out.println(point.normeMax());
+    System.out.println(Point.normeMax(2, -3));
   }
   
 }
