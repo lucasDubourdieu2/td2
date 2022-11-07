031ee63a9b65625433c5f6e009f2ac48a83dee01
LD3il <117454985+lucasDubourdieu2@users.noreply.github.com>
2022-11-07 08:53:29 +0000

    Exercice 1 partie 2

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-  public static void main(String... args) {
-    Pilote pilote = new Pilote("pilote", 18);
-    Voiture voiture = new Voiture("Kia", 7450.88, pilote);
-
-    System.out.println(voiture);
-  }
-}
---
+++ etudiant/TestEtudiant.java
@@ -0,0 +1,13 @@
+package etudiant;
+
+
+public class TestEtudiant {
+
+  public static void main(String[] args) {
+    Etudiant etudiant = new Etudiant("nom");
+    etudiant.travailler();
+    etudiant.seReposer();
+    
+  }
+  
+}
