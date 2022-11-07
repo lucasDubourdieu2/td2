b131aa2f175996e28b7157186dfc0ca4a4dc92a7
LD3il <117454985+lucasDubourdieu2@users.noreply.github.com>
2022-11-07 08:07:52 +0000

    Exercice 1

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+import java.util.*;
+
+class Chien {
+
+  private String race;
+  private String couleur;
+  private int age;
+
+  /**
+   * Renvoie une chaine de caractere "aboiement"
+   * @return aboiement, chaine de caractère "aboeiment"
+   */
+  public String aboyer() {
+    return "aboiement";
   }
+
+  /**
+   * Le chien dors
+   */
+  public void dormir() {
+    
+  }
+
+   
+
+ 
 }
